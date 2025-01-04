ccff41d10b50379e6223734d5ce049ebe9e88a8d
nhilk <nshilk@gmail.com>
2025-01-04 18:10:32 -0500

    expanding national weather service requests to gather weather data

--- weather_comp/api/nws_api.py
+++ weather_comp/api/nws_api.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_nsw(lat, long):
+def get_forcast_url(lat, long):
     if lat is None or long is None:
         raise ValueError("Latitude and Longitude are required")
     url = f"https://api.weather.gov/points/{lat},{long}"
@@ -8,10 +8,24 @@ def get_nsw(lat, long):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        data['properties']['forecast'] = 'https://api.weather.gov/gridpoints/forecast'
-        return data
+        return data['properties']['forecastHourly']
     else:
         None
 
+def get_weather_forcast(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather = response.json()
+        weather['source'] = "https://api.weather.gov"
+        return response.json()
+    else:
+        raise ValueError("Unable to get weather forcast")
+
+def get_nsw(lat, long):
+    forcast_url = get_forcast_url(lat, long)
+    if forcast_url is None:
+        return None
+    return get_weather_forcast(forcast_url)
+
 if __name__ == "__main__":
-    get_nsw(26.16123,-81.80686)
+    print(get_nsw(26.16123,-81.80686))
