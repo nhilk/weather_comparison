0b3c121df8b21a9ee1b0d8f8dccf2c327435412c
nhilk <nshilk@gmail.com>
2025-03-05 19:15:17 -0500

    Formatting

--- weather_comp/database/db.py
+++ weather_comp/database/db.py
@@ -6,17 +6,20 @@ from datetime import datetime
 import polars as pl
 import logging
 
+
 class DB:
     def __init__(self, config):
         self.logger = logging.getLogger(__name__)
-        self.logger.debug('connecting to the database')
+        self.logger.debug("connecting to the database")
         self.config = config
-        self.engine = create_engine(self.config['database']['db_url'], echo=True)
+        self.engine = create_engine(self.config["database"]["db_url"], echo=True)
         init_db(self.engine)
 
     def check_valid_filter_conditions(self, filter_conditions):
         if not isinstance(filter_conditions, (BooleanClauseList, BinaryExpression)):
-            raise TypeError(f'{type(filter_conditions)} is not a valid type for filter_conditions')
+            raise TypeError(
+                f"{type(filter_conditions)} is not a valid type for filter_conditions"
+            )
 
     def write_to_api_table(self, source, json_data, date=None):
         if not isinstance(json_data, dict):
@@ -27,14 +30,18 @@ class DB:
             if date is not None:
                 api_data_entry = ApiData(source=source, date=date, data=json_data)
             else:
-                api_data_entry = ApiData(source=source, date = datetime.now(), data=json_data)
+                api_data_entry = ApiData(
+                    source=source, date=datetime.now(), data=json_data
+                )
             session.add(api_data_entry)
 
     def read_from_api_table(self, filter_conditions=None):
         with Session(self.engine) as session, session.begin():
             if filter_conditions is not None:
                 self.check_valid_filter_conditions(filter_conditions)
-                api_data_entries = session.query(ApiData).filter(filter_conditions).all()
+                api_data_entries = (
+                    session.query(ApiData).filter(filter_conditions).all()
+                )
             elif filter_conditions is None:
                 api_data_entries = session.query(ApiData).all()
 
@@ -42,10 +49,18 @@ class DB:
 
     def write_to_fact_weather(self, data: pl.DataFrame):
         if not isinstance(data, pl.DataFrame):
-            raise TypeError(f'{type(data)} is not a valid type for writing to the database')
+            raise TypeError(
+                f"{type(data)} is not a valid type for writing to the database"
+            )
         with Session(self.engine) as session, session.begin():
             try:
-                data.write_database(table_name=str(fact_weather.__table__.schema+ '.' +fact_weather.__tablename__), connection = session, if_table_exists='append')
+                data.write_database(
+                    table_name=str(
+                        fact_weather.__table__.schema + "." + fact_weather.__tablename__
+                    ),
+                    connection=session,
+                    if_table_exists="append",
+                )
                 self.logger.info("Data written to fact_weather table")
             except Exception as e:
                 self.logger.error(f"Error writing to fact_weather table: {e}")
@@ -55,7 +70,9 @@ class DB:
         with Session(self.engine) as session, session.begin():
             if filter_conditions is not None:
                 self.check_valid_filter_conditions(filter_conditions)
-                fact_weather_entries = session.query(fact_weather).filter(filter_conditions).all()
+                fact_weather_entries = (
+                    session.query(fact_weather).filter(filter_conditions).all()
+                )
                 return fact_weather_entries
             elif filter_conditions is None:
                 fact_weather_entries = session.query(fact_weather).all()
@@ -63,11 +80,18 @@ class DB:
 
     def check_existing_location(self, lat: float, long: float) -> int:
         if not isinstance(lat, float) or not isinstance(long, float):
-            raise TypeError(f'{type(lat)} and {type(long)} are not valid types for latitude and longitude')
+            raise TypeError(
+                f"{type(lat)} and {type(long)} are not valid types for latitude and longitude"
+            )
         with Session(self.engine) as session, session.begin():
-            location_id = session.query(dim_location).filter(and_(dim_location.latitude == lat,
-                                                                         dim_location.longitude == long)).all()
-            if len(location_id)>0:
+            location_id = (
+                session.query(dim_location)
+                .filter(
+                    and_(dim_location.latitude == lat, dim_location.longitude == long)
+                )
+                .all()
+            )
+            if len(location_id) > 0:
                 return location_id[0].id
             else:
                 return None
@@ -76,20 +100,41 @@ class DB:
         if not isinstance(location_data, dict):
             self.logger.error("No data to write to the database")
             raise ValueError("No data to write to the database")
-        
+
         with Session(self.engine) as session, session.begin():
-            
-            location_id = self.check_existing_location(location_data['latitude'], location_data['longitude'])
+            location_id = self.check_existing_location(
+                location_data["latitude"], location_data["longitude"]
+            )
             if location_id is not None:
-                self.logger.info(f"Location already exists in the database with id {location_id}")
+                self.logger.info(
+                    f"Location already exists in the database with id {location_id}"
+                )
                 return location_id
             else:
                 try:
                     location_frame = pl.DataFrame(location_data)
-                    location_frame.write_database(table_name=str(dim_location.__table__.schema+ '.' +dim_location.__tablename__), connection = session, if_table_exists='append')
-                    location_id = session.query(dim_location).filter(and_(dim_location.latitude == location_data['latitude'],
-                                                             dim_location.longitude == location_data['longitude'])).all()
-                    self.logger.info(f"Location added to the database with id {location_id}")
+                    location_frame.write_database(
+                        table_name=str(
+                            dim_location.__table__.schema
+                            + "."
+                            + dim_location.__tablename__
+                        ),
+                        connection=session,
+                        if_table_exists="append",
+                    )
+                    location_id = (
+                        session.query(dim_location)
+                        .filter(
+                            and_(
+                                dim_location.latitude == location_data["latitude"],
+                                dim_location.longitude == location_data["longitude"],
+                            )
+                        )
+                        .all()
+                    )
+                    self.logger.info(
+                        f"Location added to the database with id {location_id}"
+                    )
                     return location_id[0]
                 except Exception as e:
                     self.logger.error(f"Error writing to dim_location table: {e}")
@@ -99,7 +144,9 @@ class DB:
         with Session(self.engine) as session, session.begin():
             if filter_conditions is not None:
                 self.check_valid_filter_conditions(filter_conditions)
-                dim_location_entries = session.query(dim_location).filter(filter_conditions).all()
+                dim_location_entries = (
+                    session.query(dim_location).filter(filter_conditions).all()
+                )
                 return dim_location_entries
             elif filter_conditions is None:
                 dim_location_entries = session.query(dim_location).all()
--- weather_comp/tests/database_test.py
+++ weather_comp/tests/database_test.py
@@ -2,7 +2,8 @@ import pytest
 from unittest.mock import patch, Mock
 import os
 import sys
-sys.path.append(os.path.abspath(''))
+
+sys.path.append(os.path.abspath(""))
 from database.db import DB
 from database.models import ApiData, fact_weather, dim_location
 import polars as pl
@@ -10,90 +11,102 @@ import datetime
 import logging
 
 logging.basicConfig(
-    filename='weather_comparison_db_testing.log',  # Specify the log file name
+    filename="weather_comparison_db_testing.log",  # Specify the log file name
     level=logging.DEBUG,  # Set the logging level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    filemode='w',  # Mode: 'w' to overwrite, 'a' to append
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' # Define the log message format
+    filemode="w",  # Mode: 'w' to overwrite, 'a' to append
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Define the log message format
 )
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def mock_config():
-    return {'database':{'db_url': 'postgresql://hilkshake:SomethingIsWrong@0.0.0.0:5432/test_weather_db'}}
+    return {
+        "database": {
+            "db_url": "postgresql://hilkshake:SomethingIsWrong@0.0.0.0:5432/test_weather_db"
+        }
+    }
+
+
 @pytest.fixture
 def time():
     return datetime.datetime.now()
 
+
 @pytest.fixture
 def mock_data(time):
-    
     return {
-        'properties': {
-            'forecastHourly':'https://api.weather.gov/gridpoints/MFL/52,97/forecast',
-            'periods': [
-                {
-                    'temperature': 75,
-                    'startTime': time.isoformat()
-                },
-                {
-                    'temperature': 76,
-                    'startTime': time.isoformat()
-                }
-            ]
+        "properties": {
+            "forecastHourly": "https://api.weather.gov/gridpoints/MFL/52,97/forecast",
+            "periods": [
+                {"temperature": 75, "startTime": time.isoformat()},
+                {"temperature": 76, "startTime": time.isoformat()},
+            ],
         }
     }
+
+
 @pytest.fixture
 def mock_location_data():
     return {
-        'city': 'test',
-        'state': 'test_state',
-        'country': 'test_country',
-        'latitude': 26.16123,
-        'longitude': -81.80686
+        "city": "test",
+        "state": "test_state",
+        "country": "test_country",
+        "latitude": 26.16123,
+        "longitude": -81.80686,
     }
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def test_db(mock_config):
     return DB(mock_config)
 
+
 def test_write_to_api_table_negative(test_db):
     with pytest.raises(TypeError) as context:
-        test_db.write_to_api_table('https://api.weather.gov', 'This is not a dictionary')
+        test_db.write_to_api_table(
+            "https://api.weather.gov", "This is not a dictionary"
+        )
     assert "No/incorrect data to write to the database" in str(context.value)
 
+
 def test_write_to_api_table(test_db, time, mock_data):
-    test_db.write_to_api_table('https://api.weather.gov', mock_data, time)
+    test_db.write_to_api_table("https://api.weather.gov", mock_data, time)
     data = test_db.read_from_api_table((ApiData.date == time))
     df = pl.DataFrame(data)
     assert df.shape[0] == 1
 
+
 def test_check_existing_location_negative(test_db):
     lat, long = None, None
-    #check that the function raises a ValueError when lat and long are None
     with pytest.raises(TypeError) as context:
         test_db.check_existing_location(lat, long)
     assert "are not valid types for latitude and longitude" in str(context.value)
-    
+
+
 def test_write_to_dim_location(test_db, mock_location_data):
     loc_id = test_db.write_to_dim_location(mock_location_data)
-    print(f'loc_id = {loc_id}')
+    print(f"loc_id = {loc_id}")
     data = test_db.read_from_dim_location(dim_location.latitude == 26.16123)
     df = pl.DataFrame(data)
-    print(f'data = {data}, df = {df}')
+    print(f"data = {data}, df = {df}")
     assert df.shape[0] == 1
+
 
 def test_check_existing_location(test_db, mock_location_data):
     loc_id = test_db.write_to_dim_location(mock_location_data)
     assert test_db.check_existing_location(26.16123, -81.80686) == loc_id
 
-def test_write_to_fact_weather(test_db, time,mock_data):
-    df = pl.DataFrame({
-        'date': [time.isoformat(), time.isoformat()],
-        'location_id': 1,
-        'temperature': [75, 76],
-        'pressure': [None, None],
-        'cloud_cover': [None, None]
-    })
+
+def test_write_to_fact_weather(test_db, time, mock_data):
+    df = pl.DataFrame(
+        {
+            "date": [time.isoformat(), time.isoformat()],
+            "location_id": 1,
+            "temperature": [75, 76],
+            "pressure": [None, None],
+            "cloud_cover": [None, None],
+        }
+    )
     test_db.write_to_fact_weather(df)
     df = pl.DataFrame(test_db.read_from_fact_weather(fact_weather.date == time))
     assert df.shape[0] == 2
-    
